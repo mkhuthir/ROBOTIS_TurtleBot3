88366a7d0981634b32e1cd5395cd42607babc656
mkhuthir <mkhuthir@gmail.com>
2019-10-13 22:09:12 +0400

    added example patrol

---
+++ pc/ex_patrol_client.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Patrol Example - Client
+
+roslaunch turtlebot3_example turtlebot3_client.launch
---
+++ pc/ex_patrol_server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Patrol Example - Server
+
+rosrun turtlebot3_example turtlebot3_server
