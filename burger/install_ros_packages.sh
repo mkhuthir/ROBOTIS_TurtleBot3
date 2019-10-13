bce405f6c724cee4a00ad11fd2fb4b70bc56ce61
mkhuthir <mkhuthir@gmail.com>
2019-10-13 21:55:47 +0400

    added install

---
+++ burger/install_ros_packages.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Install ROS packages for burger SBC
+
+
+cd ~/catkin_ws/src
+git clone https://github.com/ROBOTIS-GIT/hls_lfcd_lds_driver.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone https://github.com/UbiquityRobotics/raspicam_node.git
+
+cd ~/catkin_ws/src/turtlebot3
+sudo rm -r turtlebot3_description/ turtlebot3_teleop/ turtlebot3_navigation/ turtlebot3_slam/ turtlebot3_example/
+
+cd ~/catkin_ws
+catkin_make -j1
---
+++ pc/install_ros_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Install ROS packages for PC
+
+cd ~/catkin_ws/src/
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
+cd ~/catkin_ws
+catkin_make
